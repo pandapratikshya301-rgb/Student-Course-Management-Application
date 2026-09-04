db9c3fc3690e2cc973981298d77911bf8d681b40
pandapratikshya301-rgb <pandapratikshya301@gmail.com>
2026-09-04 17:08:54 +0530

    Create About component for course management app

---
+++ About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+
+      <p>
+        This application helps students view and manage
+        information about available courses.
+      </p>
+    </div>
+  );
+}
+
+export default About;
